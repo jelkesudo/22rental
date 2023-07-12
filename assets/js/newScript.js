40055221f6a1ade5ed737e81cbe8cdaa98a26518
jelkesudo <ficajelic@gmail.com>
2023-07-12 03:59:31 +0200

    Update newScript.js

--- assets/js/newScript.js
+++ assets/js/newScript.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     AOS.init();
-    console.log(window.location.pathname);
     ajaxCallBack("nav", function(result){
       printHeader(result);
       if(window.location.pathname == "/22rental/" || window.location.pathname == "/22rental/index.html"){
